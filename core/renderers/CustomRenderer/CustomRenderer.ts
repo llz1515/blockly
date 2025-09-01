8a1777a68afebef7d9e2943336ba6871a2aadd46
liamlzliang <liamlzliang@tencent.com>
2025-09-01 16:47:15 +0800

    add custom renderer

---
+++ core/renderers/CustomRenderer/CustomRenderer.ts
@@ -0,0 +1,5 @@
+import {Renderer} from './renderer.js';
+
+export {
+  Renderer
+};
---
+++ core/renderers/CustomRenderer/renderer.ts
@@ -0,0 +1,12 @@
+import {Renderer as BaseRenderer} from '../common/renderer.js';
+import * as blockRendering from '../common/block_rendering.js';
+
+export class Renderer extends BaseRenderer{
+    constructor(name: string) {
+        super(name);
+
+        console.warn("Custom renderer start!");
+    }
+}
+
+blockRendering.register('CustomRenderer', Renderer);
